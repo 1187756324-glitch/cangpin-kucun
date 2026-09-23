54fd6952938f2ed08c7e6c5aeb7849718c2faee2
1187756324-glitch <1187756324@qq.com>
2026-09-23 16:47:38 +0800

    修复弹窗关闭与分类对齐

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "collection-stock-v1";
+const CACHE = "collection-stock-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
